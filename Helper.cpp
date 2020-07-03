62caead57733b7045dba28d532e3e784580353f2
Anastasios Tzavellas <tzavellas@users.noreply.github.com>
2020-07-03 14:54:48 +0300

    Change algorithm and use binary tree (#1)
    * Implement with binary tree
    
    * Add comments

---
+++ Helper.cpp
@@ -0,0 +1,137 @@
+#include <math.h>
+#include <stdlib.h>
+#include "Helper.h"
+
+
+leaf_ptr create_leaf(void* data, size_t block_size)
+{
+	leaf_ptr ret = (leaf_ptr)malloc(sizeof(leaf_t));
+	if (ret)
+	{
+		ret->data = data;
+		ret->block_size = block_size;
+		ret->state = Available;
+		ret->left = NULL;
+		ret->right = NULL;
+	}
+	return ret;
+}
+
+
+void destroy_tree(leaf_ptr leaf)
+{
+	if (leaf)
+	{
+		destroy_tree(leaf->left);
+		destroy_tree(leaf->right);
+		free(leaf);
+	}
+}
+
+
+size_t log2(size_t x)
+{
+	return (size_t)ceil(log(x) / log(2));
+}
+
+
+leaf_ptr search(leaf_ptr leaf, void* data)
+{
+	leaf_ptr ret = NULL;
+	if (leaf)
+	{
+		if ((Full == leaf->state) && (data == leaf->data))
+		{
+			ret = leaf;
+		}
+		else
+		{
+			ret = search(leaf->left, data);
+			if (!ret)
+				ret = search(leaf->right, data);
+		}
+	}
+	return ret;
+}
+
+
+int split(leaf_ptr* leaf, void* data, size_t block_size)
+{
+	int ret = 0;
+	if (*leaf)
+	{
+		leaf_ptr left, right;
+
+		left = create_leaf(data, block_size);
+		right = create_leaf((void*)((char*)data + block_size), block_size);
+
+		if (left && right)
+		{
+			(*leaf)->left = left;
+			(*leaf)->right = right;
+			ret = 1;
+		}
+	}
+	return ret;
+}
+
+
+state_t update_leaf(leaf_ptr* leaf)
+{
+	state_t ret = Available;
+	if (*leaf)
+	{
+		if (!(*leaf)->left && !(*leaf)->right)
+		{
+			ret = (*leaf)->state;
+		}
+		else if ((*leaf)->left && (*leaf)->right)
+		{
+			state_t left = update_leaf(&(*leaf)->left);
+			state_t right = update_leaf(&(*leaf)->right);
+			if (Full == left && Full == right)
+			{
+				(*leaf)->state = Full;
+				ret = Full;
+			}
+			else if (Available == left && Available == right)
+			{
+				free((*leaf)->left);
+				(*leaf)->left = NULL;
+				free((*leaf)->right);
+				(*leaf)->right = NULL;
+				(*leaf)->state = Available;
+				ret = Available;
+			}
+			else
+			{
+				(*leaf)->state = Allocated;
+				ret = Allocated;
+			}
+		}
+		else
+		{
+			// not valid case, leaves come in pairs
+		}
+	}
+	return ret;
+}
+
+
+size_t count_allocated(leaf_ptr leaf)
+{
+	size_t ret = 0;
+	if (leaf)
+	{
+		if (!leaf->left && !leaf->right)
+		{
+			ret = (Full == leaf->state) ? leaf->block_size : 0;
+		}
+		else
+		{
+			ret = count_allocated(leaf->left);
+			ret += count_allocated(leaf->right);
+		}
+	}
+	return ret;
+}
--- buddy_allocator.h
+++ buddy_allocator.h
@@ -1,29 +1,9 @@
 #pragma once
-
-enum state_t
-{
-    Free = 0,
-    Taken,
-    Full
-};
-
-typedef struct node
-{
-    void* data;
-    struct node* next;
-    struct node* buddy;
-    struct node* parent;
-    state_t state;
-}node_t;
-
-typedef node_t* node_ptr;
+#include "Helper.h"
 
 typedef struct buddy_allocator
 {
-    size_t memory_size;
-    void* ptr;
-    node_ptr* heads;
-    size_t heads_size;
+    leaf_ptr root;
 }buddy_allocator_t;
 
 buddy_allocator_t* buddy_allocator_create(void* raw_memory, size_t raw_memory_size);
